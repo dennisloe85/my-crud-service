a52878be4a4fcc36cb8ab4a913404d2382488e82
Dennis <dennis@loeben.de>
2020-06-17 19:38:15 +0200

    DB configuration for Heroku

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "malibu88",
-    DB: "testdb",
+    HOST: "ec2-54-247-79-178.eu-west-1.compute.amazonaws.com",
+    USER: "ppcxlptzvngucv",
+    PASSWORD: "f894bda3375da53be9bad9ea3b0594d365e9b9f4436f72f387d7f36e0ed2c161",
+    DB: "d9kk7gn5spki6f",
     dialect: "postgres",
     pool: {
       max: 5,
